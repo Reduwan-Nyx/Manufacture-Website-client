f83170f8bf653912a3ab8ff88a25353b674391d7
Reduwan-Nyx <arnodorain124@gmail.com>
2022-05-24 23:51:34 +0600

    admin protected completed

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import MyPurchase from "./Pages/Dashboard/MyPurchase";
 import MyReview from "./Pages/Dashboard/MyReview";
 import MyProfile from "./Pages/Dashboard/MyProfile";
 import Users from "./Pages/Dashboard/Users";
+import RequireAdmin from "./Pages/Login/RequireAdmin";
 
 
 function App() {
@@ -47,7 +48,9 @@ function App() {
           <Route index element={<MyPurchase></MyPurchase>}></Route>
           <Route path="review" element={<MyReview></MyReview>}>
             </Route> <Route path="myprofile" element={<MyProfile></MyProfile>}>
-            </Route> <Route path="users" element={<Users></Users>}></Route> 
+            </Route> <Route path="users" element={<RequireAdmin>
+              <Users></Users>
+            </RequireAdmin>}></Route> 
         </Route>
 
         <Route path="protfolio" element={<MyProtfolio />} />
